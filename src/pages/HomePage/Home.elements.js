b5c786a611b8fe24fa73a6fe1639e826f0feb9d6
Ayoub R7C <ayoubch34@gmail.com>
2021-07-28 18:29:50 +0100

    height of page

--- src/pages/HomePage/Home.elements.js
+++ src/pages/HomePage/Home.elements.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 export const MainHomeContainer = styled.main`
   width: calc(100% - 315px);
-  /* height: 100vh; */
+  height: 100%;
   position: relative;
   left: 315px;
   top: 0;
   background: ${({ theme }) => theme.mainBg};
   padding: 1em;
-
+  @media screen and (min-height: 1000px) {
+    height: 100vh;
+  }
   @media screen and (max-width: 966px) {
     left: 0;
     width: 100%;
